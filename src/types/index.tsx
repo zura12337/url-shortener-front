95f804e54c51fca3fb050dd451e363ced2ef9e9d
Zura Kurtanidze <zura.kurta@gmail.com>
2021-06-30 14:29:37 +0400

    :label: Create types for url and user.

---
+++ src/types/index.tsx
@@ -0,0 +1,14 @@
+export interface UrlType {
+  originalUrl: string;
+  id: string;
+  shortUrl: string;
+  visitors: [{ date: string; ip: string }];
+  uniqueVisitors: [{ date: string; ip: string }];
+  generatedBy: [string];
+  date: string;
+}
+
+export interface UserType {
+  ip: string;
+  visitedLinks: [string];
+}
