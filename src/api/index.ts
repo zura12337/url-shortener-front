21bcb267b01065a590f17bafc3490d2400144561
Zura Kurtanidze <zura.kurta@gmail.com>
2021-07-06 21:11:58 +0400

    :sparkles: Add endpoint to get user visited urls.

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,13 +3,13 @@
 import axios from "axios";
 import useSWR from "swr";
 
-const apiUrl = "https://url-shortener-api-z.herokuapp.com"
+const apiUrl = "https://url-shortener-api-z.herokuapp.com/api"
 
 const fetcher = (url: string) => axios(url).then((res) => res.data);
 
 export async function shortenUrl(url: string) {
   try {
-    const response = await axios.post(`${apiUrl}/`, { url });
+    const response = await axios.post(`${apiUrl}/url`, { url });
     return response;
   } catch (ex) {
     return ex.response;
@@ -17,12 +17,22 @@ export async function shortenUrl(url: string) {
 }
 
 export function getUrlById(id: string) {
-  const { data, error } = useSWR(`${apiUrl}/${id}`, fetcher);
+  const { data, error } = useSWR(`${apiUrl}/url/${id}`, fetcher);
 
   return {
     data,
     error,
   };
+}
+
+export function getMyVisitedLinks() {
+  const { data, error } = useSWR(`${apiUrl}/visited`, fetcher);
+
+  return {
+    data,
+    isLoading: !data && !error,
+    error
+  }
 }
 
 export function getMyUrls() {
