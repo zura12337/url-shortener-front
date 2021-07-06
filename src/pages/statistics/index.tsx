140386cce2ac93318e54aafefe05b834c45ff96d
Zura Kurtanidze <zura.kurta@gmail.com>
2021-07-07 00:24:34 +0400

    :boom: Add dropdown to statistics page and show if link is paused or removed.

--- src/pages/statistics/index.tsx
+++ src/pages/statistics/index.tsx
@@ -1,12 +1,14 @@
-import { Link, Box, Text, Flex, Grid, Divider, Image } from "@chakra-ui/react";
+import { Link, Box, Text, Flex, Grid, Divider, Image, Button, useToast } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import { getUrlData } from "../../api";
+import { getUrlData, editUrl } from "../../api";
 import { UrlMetadataType, UrlType } from "../../types";
 import { useHistory } from "react-router-dom";
 import SecondaryButton from "../../components/SecondaryButton";
 import Loading from "../../components/Loading";
 import Chart from "../../components/Chart";
 import ImagePlaceholder from '../../assets/image_placeholder.jpg';
+import {Dropdown} from "../../components/Dropdown";
+import PrimaryButton from "../../components/PrimaryButton";
 
 export default function StatisticsPage({ match }: { match: any }) {
   const id = match.params.id;
@@ -14,12 +16,15 @@ export default function StatisticsPage({ match }: { match: any }) {
   const { data, isLoading: loading } = getUrlData(id);
   const [urlData, setUrlData] = useState<UrlType>();
   const [urlMetadata, setUrlMetadata] = useState<UrlMetadataType>();
+  const [expanded, setExpanded] = useState<boolean>(false);
+  const toast = useToast();
+
 
   useEffect(() => {
     if (data) {
       setUrlData(data.url);
       setUrlMetadata(data.metadata);
-    }
+    } 
   }, [data]);
 
   return (
@@ -46,7 +51,7 @@ export default function StatisticsPage({ match }: { match: any }) {
           <Loading />
         </Grid>
       )}
-      {urlData && (
+      {urlData && urlData.status !== "remove" ? (
         <>
           <Text color="white" fontSize={30} mb={5} float="right">
             Link Analytics
@@ -63,19 +68,17 @@ export default function StatisticsPage({ match }: { match: any }) {
             gridGap={5}
             alignItems="center"
           >
-            {urlMetadata && (
-              <Image
-                src={urlMetadata.image || ImagePlaceholder}
-                alt={urlMetadata.title}
-                borderRadius="10px"
-                h="max-content"
-                maxH="100%"
-                objectFit="cover"
-                w="140px"
-              />
-            )}
+            <Image
+              src={urlMetadata?.image || ImagePlaceholder}
+              alt={urlMetadata?.title}
+              borderRadius="10px"
+              h="max-content"
+              maxH="100%"
+              objectFit="cover"
+              w="140px"
+            />
             <Box>
-              {urlMetadata && <Text mb={2}>{urlMetadata.title}</Text>}
+              <Text mb={2}>{urlMetadata?.title}</Text>
               <Text color="gray.500">{urlData.originalUrl}</Text>
               <Divider my={3} />
               <Link color="blue.600" href={urlData.shortUrl} _focus={{}}>
@@ -92,12 +95,54 @@ export default function StatisticsPage({ match }: { match: any }) {
                 right="10px"
                 borderRadius={5}
                 fontSize={14}
+                height="31px"
               >
                 {urlData.date}
               </Box>
+              <Dropdown position="absolute" top="10px" right="110px">
+                  {urlData.status === "active" ? (
+                    <Button _hover={{}} _active={{}} bg="none" w="max-content" h="max-content" _focus={{}} p={0} fontSize={12} onClick={async () => {
+                        const response = await editUrl({ id, action: "pause" })
+                        if(response.statusText === "OK") {
+                          toast({
+                            title: "Link has been paused",
+                            isClosable: true,
+                          })
+                        } 
+                      }}
+                    >
+                      Pause Link
+                    </Button> 
+                  ) : (
+                    <Button _hover={{}} _active={{}} bg="none" w="max-content" h="max-content" _focus={{}} p={0} fontSize={12} onClick={async () => {
+                        const response = await editUrl({ id, action: "unpause" })
+                        if(response.statusText === "OK") {
+                          toast({
+                            title: "Link has been unpaused",
+                            isClosable: true,
+                          })
+                        } 
+                      }}
+                    >
+                      Unpause Link
+                    </Button> 
+                  )}
+                  <Button _hover={{}} _active={{}} bg="none" _focus={{}} p={0} fontSize={12}  h="max-content" w="max-content" onClick={async () => {
+                      const response = await editUrl({ id, action: "remove" });
+                      if(response.statusText === "OK") {
+                        toast({
+                          title: "Link has been removed",
+                          isClosable: true,
+                        })
+                      }
+                    }}
+                  >
+                    Remove Link
+                  </Button> 
+              </Dropdown>
             </Box>
           </Flex>
-          {urlData.visitors.length > 0 ? (
+          {urlData.visitors.length > 0 && urlData.status !== "pause" ? (
             <>
               <Grid
                 maxW="100%"
@@ -146,13 +191,30 @@ export default function StatisticsPage({ match }: { match: any }) {
                 />
               </Grid>
             </>
-          ) : (
+          ) : urlData.visitors.length === 0 ? (
             <Text textAlign="center" mt={50} fontSize={32} color="white">
               No one visited this link yet
             </Text>
+          ) : (
+            <Flex justifyContent="center" alignItems="center" direction="column" gridGap={5} bg="white" borderRadius={9} mt="15px" py={10}>
+              <Text textAlign="center" fontSize={32}>
+                This URL is paused
+              </Text>
+              <SecondaryButton bg="white" label="Unpause" onClick={async () => {
+                const response = await editUrl({ id, action: "unpause" })
+                if(response.statusText === "OK") {
+                  toast({
+                    title: "Link has been unpaused",
+                    isClosable: true,
+                  })
+                } 
+              }}/>
+            </Flex>
           )}
         </>
-      )}
+      ) : urlData && urlData.status === "remove" ? (
+        <Text fontSize={32} textAlign="center" color="white" fontWeight="bold" mt="10%">This link has been removed by owner</Text>
+      ) : (<></>)}
     </Box>
   );
 }
